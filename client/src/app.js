cc0f2a183e76b2c49bf6ab1381e292147cbb50cc
Richard Edwards <richardedwards899@gmail.com>
2017-07-24 12:59:41 +0100

    change background file

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import Home from './components/Home'
 import Listing from './components/Listing'
-// import {Router, Route, IndexRoute, hashHistory} from 'react-router'
 import { HashRouter, Route, IndexRoute } from 'react-router-dom'
 
 
--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.get('/', function (req, res) {
 app.use(express.static('client/build'));
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Front end app listening at http://%s:%s', host, port);
 });
